980bc44676065e6005f1a331f31e16b8ae6b06fa
mh <mheathcock@hotmail.co.uk>
2025-11-23 16:39:07 +0000

    Continued work on GUI and track analysis logic , -Fixed outdated button display names
    -Implemented GUI and logic for analysing tracks from filtered playlists.
    --Filter works by searching for playlists with the current year in the title.
    
    -Implemented GUI for allowing the user to name their playlist and a toggle button for appending the current month + year to playlist name.
    
    -Removed outdated code and comments

--- SpotifyMonthly.py
+++ SpotifyMonthly.py
@@ -3,40 +3,16 @@ from tkinter import *
 from tkinter import messagebox 
 import customtkinter as CTk
 from PIL import Image, ImageTk
-from auth import *
+import datetime
+
 app = CTk.CTk()
 app.geometry("1920x1080")
 app.configure(bg="#121212")
 app.state('zoomed')  
 
-
-"""
-Front end GUI for user authentication.
-"""
-def login_gui():
-    #forget previous widgets
-    for widget in app.winfo_children():
-        widget.destroy()
-    
-  
-    app_frame = CTk.CTkFrame(app, fg_color="transparent")
-    app_frame.pack(expand=True,anchor="center")
-
-    app.title("Login")
-    
-   
-    CTk.CTkButton(
-        app_frame, 
-        text="Login", 
-        command=login_gui, 
-        width=250, 
-        height=50, 
-        font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="bold"),
-        corner_radius=30,
-        fg_color="#BB86FB", 
-        hover_color="#9C5AF7"
-    ).pack(pady=10)
-     
+#Variable for setting a boolean that determines if the Users playlist should contain a month + year
+date_toggle_var = CTk.BooleanVar(value=True)
+track_ids = []
 
 
 """
@@ -88,9 +64,9 @@ def login_gui():
        
 
     #Login button that calls the start_spotify_login function when clicked
-    CTk.CTkButton(
+    Login_button = CTk.CTkButton(
         login_frame, 
-        text="SUBMIT",
+        text="LOGIN TO SPOTIFY",
         font=CTk.CTkFont(family="Helvetica Neue LT Std",size=40, weight="bold"),
         text_color="white",
         width=200, 
@@ -99,27 +75,83 @@ def login_gui():
         fg_color="black", 
         hover_color="#333333",
         command=start_spotify_login
-    ).pack(pady=(20, 5))
+    )
+    Login_button.pack(pady=(20, 5))
+    
   
+def MainGUI(spotify_client):
+    for widget in app.winfo_children():
+        widget.destroy()
+    main_frame = CTk.CTkFrame(app, fg_color="transparent")
+    main_frame.pack(expand=True, fill="both")
+    
+    button_container = CTk.CTkFrame(main_frame, fg_color="transparent")
+    button_container.pack(pady=50, expand=True, anchor="center")
 
+    view_track_button = CTk.CTkButton(
+        master=button_container, 
+        text="View Tracks", 
+        width=250, 
+        height=50, 
+        font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="bold"),
+        corner_radius=30,
+        fg_color="#BB86FB", 
+        hover_color="#9C5AF7",
+        text_color="white",
+        command=lambda: Show_Track_GUI(spotify_client)
+    )
+    view_track_button.pack(side='left', padx=15, pady=10)
+   
+    analyse_track_button = CTk.CTkButton(
+        master=button_container, 
+        text="Analyse Tracks", 
+        width=250, 
+        height=50, 
+        font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="bold"),
+        corner_radius=30,
+        fg_color="#BB86FB", 
+        hover_color="#9C5AF7",
+        text_color="white",
+        command=lambda: Analyse_Track_GUI(spotify_client)
+    )
+    analyse_track_button.pack(side='right', padx=15, pady=10)
+  
   
 """
 Main GUI Function for the app, is called once user is 
 logged in and uses their credentials to set the config files and call the functions to display spotify info
 """      
-def MainGUI(spotify_client):
+def Show_Track_GUI(spotify_client):
+
+
+    global Playlist_entry_name
+  
+   
+
     #Forget previous widgets
     for widget in app.winfo_children():
         widget.destroy()
-    app.title("Main Application Dashboard")
+
+    app.title("Your Top 20 Tracks")
 
     #Create a central frame to hold all content
     main_frame = CTk.CTkFrame(app, fg_color="transparent")
     main_frame.pack(expand=True, fill="both")
-    
+   
+    #Back button
+    CTk.CTkButton(
+        master=main_frame, 
+        text="Back", 
+        width=100, 
+        height=30, 
+        command=lambda: MainGUI(spotify_client),
+        fg_color="gray", 
+        hover_color="darkgray"
+    ).pack(side="top", anchor="nw", pady=10, padx=10)
+
     try:
         user_info = spotify_client.current_user()
-        username = user_info.get('display_name', 'Your Friend')
+        username = user_info.get('display_name', '')
         
 
         CTk.CTkLabel(
@@ -128,20 +160,115 @@ def MainGUI(spotify_client):
             font=CTk.CTkFont(family="Helvetica Neue LT Std", size=40, weight="bold"),
             text_color="#BB86FB"
         ).pack(pady=(40, 20))
-        
-        display_top_tracks(main_frame, spotify_client) 
-        
+   
     except Exception as e:
         messagebox.showerror("Authentication Error", f"Access denied or token expired: {e}")
         login_gui()
+    
+    content_frame  = CTk.CTkFrame(
+        main_frame,
+        fg_color="transparent"
+    )
+    content_frame.pack(fill="both", expand=True, padx=20, pady=10)
+
+    content_frame.grid_columnconfigure(0, weight=3)
+    content_frame.grid_columnconfigure(1, weight=1)
+    content_frame.grid_rowconfigure(0, weight=1)
+
+    display_top_tracks(content_frame, spotify_client) 
+
+    controls_frame = CTk.CTkFrame(
+        content_frame,
+        fg_color="transparent"
+    )
+    controls_frame.grid(row=0, column=1, padx=20, pady=20, sticky="n")
+    
+    
+    CTk.CTkLabel(
+            controls_frame, 
+            text=f"Write a name for your Playlist!", 
+            font=CTk.CTkFont(family="Helvetica Neue LT Std", size=20, weight="bold"),
+            text_color="#BB86FB"
+        ).pack(pady=(20, 10))
+    
+    #Switch responsible for letting the user choose if they want the playlist name to include current month + year
+    CTk.CTkSwitch(
+        master=controls_frame,
+        text="Add current Month + Year for analysis functions",
+        font=CTk.CTkFont(size=16),
+        fg_color="#1B1A1B", #Colour for OFF
+        progress_color="#BB86FB", #Colour for ON
+        variable=date_toggle_var, 
+        onvalue=True,
+        offvalue=False
+    ).pack(pady=(5, 15), padx=50)
+
+    #Entry box for the user to input a playlist name
+    Playlist_entry_name = CTk.CTkEntry(
+    master=controls_frame,
+    placeholder_text="Enter Playlist Name",
+    font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="normal"),
+    width=450,
+    height=60,
+    corner_radius=30, 
+    fg_color="white",
+    text_color="#171717",
+    border_width=0
+    )
+    Playlist_entry_name.pack(pady=(20, 10), padx=50)
+
+    CTk.CTkButton(
+        master=controls_frame, 
+        text="Create Playlist", 
+        width=250, 
+        height=50, 
+        font=CTk.CTkFont(family="Helvetica Neue LT Std",size=30, weight="bold"),
+        corner_radius=30,
+        fg_color="#BB86FB", 
+        hover_color="#9C5AF7",
+        text_color="white",
+        command=lambda: save_playlist(spotify_client)
+    ).pack(pady=10)
+
+"""Logic function for saving the playlist to users Spotify account"""
+def save_playlist(spotify_client):
+    global Playlist_entry_name, track_ids,date_toggle_var 
+
+    playlist_name = Playlist_entry_name.get() #get the name the user inputted in Show_Track_GUI - Playlist_entry_name CTk Entry
+    add_date_to_name = date_toggle_var.get()
+
+    if playlist_name == "":
+        if add_date_to_name:
+            messagebox.showerror("Warning!", "No Playlist name entered, default will be the current month + year")
+        else:
+            messagebox.showerror("Warning!", "No Playlist name entered, default will be 'My Playlist'")
+
+        Playlist_entry_name.configure(border_color="red", border_width=3)
+      
+    
+    if not track_ids:
+        messagebox.showwarning("Warning!", "No top tracks were retrieved. Cannot fill playlist.")
+        return
+    
+    try:
+        create_and_fill_playlist(spotify_client, track_ids, playlist_name, add_date_to_name)#Call function from main.py that will create and populate the playlist with the top 20 tracks
+    except Exception as e:
+        messagebox.showerror("Spotify API Error", f"Could not create playlist: {e}")
+    else:
+        messagebox.showinfo("Success!", "Playlist created!")
+        Playlist_entry_name.configure(border_color="#9C5AF7", border_width=0)
+        Playlist_entry_name.delete(0, CTk.END)
+
+
     """
     Fetches top tracks, formats them, and displays them in a CTkTextbox.
     """
 def display_top_tracks(app_frame, spotify_client):
-
+    global track_ids
     
     try:
         raw_tracks = get_top_tracks(spotify_client)
+        
     except Exception as e:
         messagebox.showerror("Spotify API Error", f"Could not fetch top tracks: {e}")
         return
@@ -150,20 +277,21 @@ def display_top_tracks(app_frame, spotify_client):
     if not raw_tracks:
         formatted_output += "No recent track data found for this account."
     else:
+        track_ids = [item[0] for item in raw_tracks]
         for i, (track_id, track_name, artists) in enumerate(raw_tracks):
             artists_str = ", ".join(artists)
             formatted_output += f"{i+1}. {track_name} — {artists_str}\n"
 
     track_textbox = CTk.CTkTextbox(
         master=app_frame,
-        width=800,
+        width=500,
         height=500,
-        font=CTk.CTkFont(family="Arial", size=18),
+        font=CTk.CTkFont(family="Helvetica Neue LT Std", size=18),
         fg_color="#1F1F1F", 
         text_color="white",
         wrap="word" #Wrap long lines
     )
-    track_textbox.pack(pady=20, padx=20, fill="both", expand=True)
+    track_textbox.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
  
     track_textbox.insert("0.0", formatted_output)
@@ -171,7 +299,156 @@ def display_top_tracks(app_frame, spotify_client):
  
     track_textbox.configure(state="disabled")   #Make the textbox read-only after insertion
 
+"""
+Logic for analysing the tracks.
+ This is based on if the user has been saving playlists with the year attached as this will be the filter
+ """
+def analyse_track(spotify_client):
+    
+  
+    all_playlists = []
+    
+     #Uses pagination for playlists
+    results = spotify_client.current_user_playlists(limit=50)  #Fetch inital page
+    all_playlists.extend(results.get('items', []))#Extend the list to all these fetched playlists
+    
+    #Loop as long as Spotify indicates there is a 'next' page
+    while results.get('next'):
+        try:
+            results = spotify_client.next(results)
+            all_playlists.extend(results.get('items', []))
+            
+        except Exception:
+           #Fail on error during pagination rather than crashing the GUI
+            break
+
+
+    current_year = datetime.datetime.now().year#use the current year as our filter
+    
+    #once all playlists from user have been searched apply filter to get the relevant playlists for analysis
+    target_playlists = [
+        p for p in all_playlists 
+        
+        if isinstance(p, dict) and #ensure p is a dict
+        #check if the filter (current year) is in the playlist name
+        f"{current_year}" in p.get('name', '').upper()# ('name', '') will default to empty string if playlist is missing a name preventing keyerror
+    ]
+    
+   
+    
+    artist_counts = {}
+    
+    for playlist in target_playlists:
+        tracks_results = spotify_client.playlist_items(playlist['id']) #Fetch the first page of tracks for the current playlist
+        
+        #Aggregate all tracks for this playlist use pagination 
+        all_tracks = tracks_results.get('items', [])#default 100 tracks
+        
+        #Loop as long as Spotify indicates there is a 'next' page of tracks
+        while tracks_results.get('next'):
+            try:
+                tracks_results = spotify_client.next(tracks_results)
+                all_tracks.extend(tracks_results.get('items', []))
+            except Exception:
+                #Fail on track pagination error
+                break
+            
+        #Count artists from ALL retrieved tracks
+        for item in all_tracks:
+            #Safely check if track key exists to skip local files or null tracks
+            track_data = item.get('track')
+            if track_data and track_data.get('artists'):#Check that the track data is valid and has an 'artists' list
+                for artist in track_data['artists']:#Iterates through every artist assosciated incase of collab or features 
+                    artist_name = artist['name']#Extract artist(s) name
+                    artist_counts[artist_name] = artist_counts.get(artist_name, 0) + 1 #count is 0 if artist is new 
+                    
+    #Sort and return results
+    top_artists = sorted(artist_counts.items(), key=lambda item: item[1], reverse=True)
+    return top_artists
+
+"""
+GUI for displaying these analysed artists
+"""
+def Analyse_Track_GUI(spotify_client):
+    #Forget previous widgets
+    for widget in app.winfo_children():
+        widget.destroy()
+
+    app.title("Analyse Tracks")
+
+    main_frame = CTk.CTkFrame(app, fg_color="transparent")
+    main_frame.pack(expand=True, fill="both")
+    #Back button 
+    CTk.CTkButton(
+        master=main_frame, 
+        text="Back", 
+        width=100, 
+        height=30, 
+        command=lambda: MainGUI(spotify_client),
+        fg_color="gray", 
+        hover_color="darkgray"
+    ).pack(side="top", anchor="nw", pady=10, padx=10)
+
+    #Loading message as analysis takes time
+    loading_label = CTk.CTkLabel(
+        master=main_frame,
+        text="Analysing Playlists...", 
+        font=CTk.CTkFont(family="Helvetica Neue LT Std", size=25, weight="bold"),
+        text_color="#BB86FB"
+    )
+    loading_label.pack(pady=(200, 20))
+    app.update() #Force the GUI to update and show the loading message immediately
+
+    #Start analysis func
+    try:
+        output = analyse_track(spotify_client)#call the function that handles analysing playlists based of current year filter
+    except Exception as e:
+        messagebox.showerror("Analysis Error", f"An error occurred during analysis: {e}")
+        MainGUI(spotify_client)
+        return
+
+    loading_label.destroy()#Remove loading message
+
+    
+
+    CTk.CTkLabel(
+        master=main_frame,
+        text=f"YOUR TOP ARTISTS FROM RECAP PLAYLISTS\n",
+        font=CTk.CTkFont(family="Helvetica Neue LT Std", size=25, weight="bold"),
+        text_color="#BB86FB"
+        ).pack(pady=(20, 10))
+
+
+    
+
+    formatted_output = ""
+    if not output:#if the user hasn't been using the toggle for appending the month + year there will be no tracks to analyse
+        formatted_output += "\n No artists found in the target playlists."
+    else:
+        for i, (artist_name, count) in enumerate(output):
+            formatted_output += f"\n {i+1}. {artist_name} ({count} tracks)\n"
+
+    track_textbox = CTk.CTkTextbox(
+        master=main_frame,
+        width=500,
+        height=800,
+        font=CTk.CTkFont(family="Helvetica Neue LT Std", size=18),
+        fg_color="#1F1F1F", 
+        text_color="white",
+        wrap="word" #Wrap long lines
+    )
+    track_textbox.pack(padx=20, pady=20, fill="y", expand=True)
+
+ 
+    track_textbox.insert("0.0", formatted_output)
+    
+ 
+    track_textbox.configure(state="disabled")
+    
+    
 
 login_gui()
 app.mainloop()
+
+
 
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import ast
-import os
 import os
 from dotenv import load_dotenv
+import datetime
+
+
 
 load_dotenv()
 #Config
 CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", "enter id") 
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", "enter secret")
 REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI", "http://127.0.0.1:8080")
-SCOPE="user-top-read playlist-modify-private playlist-modify-public"#this scope allows for reading top tracks and creating/modifying playlists
+SCOPE = "user-top-read playlist-modify-public playlist-read-private playlist-read-collaborative"#this scope allows for reading top tracks and creating/modifying playlists
 
 """
 Set client variables, called in SpotifyMonthly.py 
@@ -46,64 +47,32 @@ def get_top_tracks(sp_client):
 """
 Create playlist with a title and description
 """
-def create_and_fill_playlist(sp_client, track_ids):
+def create_and_fill_playlist(sp_client, track_ids, Playlist_name, date_toggle_state):
     user_id = sp_client.current_user()["id"] #get user ID
-    name    = input("\nEnter a name for your new playlist: ").strip() #.strip() removes any leading/trailing whitespace
-    if not name: #if nothing was entered
-        print("Playlist name cannot be empty. Aborting.") 
-        return
+    name_input = Playlist_name.strip() #.strip() removes any leading/trailing whitespace
+   #Date toggle is turned ON
+    if date_toggle_state == True:
+        #get current month + year 
+        current_datetime = datetime.datetime.now()
+        date_string = current_datetime.strftime("%B %Y")#make it into a readable string
+
+        if not name_input: #if nothing was entered Playlist name is simply the month + year
+            name = f"{date_string}"
+        else:
+            name = f"{name_input} {date_string}"#Playlist name is user input + month + year
+    #Date toggle is turned OFF
+    else:
+        if not name_input: #if nothing was entered
+            name = "My Playlist"#set a default playlistname
+        else:
+            name = name_input#Users input 
+
     #Creates Playlist
     playlist = sp_client.user_playlist_create( #playlist has the properties: id, Playlist Name, Public or Private, Playlist Description
         user=user_id, 
         name=name, 
-        public=False, #private playlist
+        public=True, 
         description="top 20 songs"
     )
     sp_client.playlist_add_items(playlist_id=playlist["id"], items=track_ids) #track_ids from get_top_tracks() is passed here to add the songs to the playlist
     print(f"Added {len(track_ids)} tracks to playlist: {name}")
-
-"""
-Rework started to implement these into the GUI for data analysis 
-
-
-Write the artist list to a text file 
-"""
-def write_artist_list(artist_list, file_path):
-    artist = [name[-2:] for name in artist_list]#removes the track ID and instead just keeps the name and artist
-    with open(file_path, 'a', encoding='utf-8') as file: #opens file in append mode
-        for song_artist in artist:#iterates through the artist list 
-           file.write("".join(str(song_artist)) + '\n')#writes each song + artist to a new line
-        print("Saved")
-"""
-Read the artist list from a text file and count occureneces of artists
-"""
-def read_artist_list(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file: # Opens file in read mode
-        Artist_dict = {} # empty dictionary to store the artist : count 
-        Artist = [] #empty list to store artist names as they are read from the file
-        
-        for line in file: # for each line in the text file
-            track_name, artist_list = ast.literal_eval(line.strip()) # converts string representation of list back to actual list
-            for artist_name in artist_list: #iterate through each artist in the list
-                if artist_name in Artist: # if the artist is already been seen
-                    Artist_dict[artist_name] += 1 #add +1 to that artist's count in the dictionary
-                else: # the artist hasnt been seen yet
-                    Artist.append(artist_name) # add this artist to the list of artists as it has now been seen
-                    Artist_dict[artist_name] = 1 # add the artist to the dictionary and set its count to 1
-        
-        sorted_artists = sorted(Artist_dict.items(), key=lambda x: x[1], reverse=True) #sort the dictionary by count in descending order
-        lines = [f"{artist}: {count}" for artist, count in sorted_artists] # format dictionary items into string as "artist: count"
-        return '\n'.join(lines) # add a new line between each artist
-    
-
-if __name__ == "__main__":
-    top20=get_top_tracks()#get the tracks
-    save = input("\nWould you like to save these to a new private playlist? (y/n): ").strip().lower()#ask user if they want to save the tracks
-    if save == 'y':
-        write_artist_list(top20, 'artist_list.txt')#save this data to a file to use for analysis
-        with open('artist_count.txt', 'w', encoding='utf-8') as file:
-            file.write(str(read_artist_list('artist_list.txt')))
-        track_ids = [item[0] for item in top20]#gather track ids
-        create_and_fill_playlist(track_ids)#create into playlist
-    else:
-        pass
